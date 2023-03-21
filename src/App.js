44ae4d1834579ec9e84bbe1d6c86f7ae9d42a213
tirahman105 <tirahman105@gmail.com>
2023-03-21 19:14:32 +0600

    47-5 (advanced) Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 1111;
-const doctor = {name : 'Dr. Tareq', designation:'BDS, PGT, Lecture, PDC'}
+const doctors = [
+  {name : 'Dr. Tareq', designation:'BDS, PGT, Lecture, PDC'},
+  {name : 'Dr. Riad', designation:'BDS, PGT, Senior Lecture, PDC'},
+  {name : 'Dr. Farid', designation:'BDS, FCPS, Professor, PDC'},
+  {name : 'Dr. Mithila', designation:'BDS, PGT, Lecture, PDC'}
+]
 
 const divStyle ={
   backgroundColor : 'white',
@@ -10,13 +15,24 @@ const divStyle ={
 } 
 
 function App() {
+  const names = ['Tareq', 'Anonna', 'Tahsin', 'Porag', 'Nuha']
   return (
     <div className="App">
-      <Person name="Dr. Tareq" profession="Dental Surgeon" ></Person>
-      <Person name="Anonna Ferdaus" profession="software Engineer"></Person>
+
+      {
+        names.map(name => <Person name={name}></Person>)
+      }
+
+      {
+        doctors.map(doctor => <Department name={doctor.name}></Department>)
+      }
+
+
+      {/* <Person name="Dr. Tareq" profession="Dental Surgeon" ></Person>
+      <Person name="Anonna Ferdaus" profession="software Engineer"></Person> */}
       <h3>New Component</h3>
-      <Department email="dr.tareq8092@gmail.com" ></Department>
-      <Department email="tirahman105@gmail.com" ></Department>
+      {/* <Department email="dr.tareq8092@gmail.com" ></Department>
+      <Department email="tirahman105@gmail.com" ></Department> */}
     </div>
   );
 }
@@ -33,7 +49,7 @@ function Person(props){
 function Department(props){
   return(
     <div className="container">
-      <h2>{props.email}</h2>
+      <h2>{props.name}</h2>
     </div>
   )
 }
